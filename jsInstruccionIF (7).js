c84588937a1897fed09820089d601cd29bf99d76
ceciliaybarrola <60697915+ceciliaybarrola@users.noreply.github.com>
2020-02-07 17:55:18 -0300

    Update jsInstruccionIF (7).js

--- jsInstruccionIF (7).js
+++ jsInstruccionIF (7).js
@@ -2,7 +2,17 @@ function mostrar()
 {
 //tomo la edad  
 
-	
+var edad;
+var estado;
+
+edad=document.getElementById("edad").value;
+estado=document.getElementById("estadoCivil").value;
+
+if (edad < 18 && estado != "Soltero")
+{
+    alert("Eres muy pequeño para NO ser soltero.");
+}
+
 
 
 }//FIN DE LA FUNCIÓN
