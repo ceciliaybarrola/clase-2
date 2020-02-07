55d101771b04976fd0de9af52cfad1d1518349fe
ceciliaybarrola <60697915+ceciliaybarrola@users.noreply.github.com>
2020-02-07 17:53:49 -0300

    Update jsInstruccionIF (5).js

--- jsInstruccionIF (5).js
+++ jsInstruccionIF (5).js
@@ -1,6 +1,12 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
+edad=document.getElementById("edad").value;
 
+if (edad >= 17 || edad <= 13 )
+{
+    alert("No es adolescente");
+}
 
 }//FIN DE LA FUNCIÓN
