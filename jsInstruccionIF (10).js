0d4a80723c7e63d7eb1ad334d8ef301cc455a0dd
ceciliaybarrola <60697915+ceciliaybarrola@users.noreply.github.com>
2020-02-10 15:56:47 -0300

    Update jsInstruccionIF (10).js

--- jsInstruccionIF (10).js
+++ jsInstruccionIF (10).js
@@ -2,5 +2,24 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+	var num;
+
+	num=Math.floor(Math.random(1)*10 + 1);
+
+	if (num >= 9)
+	{
+		alert("Exelente");
+	}
+	else
+	{
+		 if (num <= 4)
+		 {
+			 alert("Aprobó")
+		 }
+		 else
+		 {
+			 alert("Desaprobó")
+		 }
+	}
 
 }//FIN DE LA FUNCIÓN
